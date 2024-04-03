c436069f5d3e86204f64fb575dafd458894f7012
birkholz-cubert <142295759+birkholz-cubert@users.noreply.github.com>
2024-04-03 14:21:08 +0200

    Draft: Feature/copy documentation (#14)
    * testing cuvis copydoc functionality
    
    * ParamSpec can be in two different locations
    
    * added copy doc decorators to AcquisitionContext

---
+++ cuvis/doc.py
@@ -0,0 +1,35 @@
+# taken from https://stackoverflow.com/questions/68901049/copying-the-docstring-of-function-onto-another-function-by-name
+
+from typing import Callable, TypeVar, Any, TypeAlias
+try:
+    from typing_extensions import ParamSpec
+except ImportError as exc:
+    from typing import ParamSpec
+
+
+
+T = TypeVar('T')
+P = ParamSpec('P')
+WrappedFuncDeco: TypeAlias = Callable[[Callable[P, T]], Callable[P, T]]
+
+
+def copydoc(copy_func: Callable[..., Any]) -> WrappedFuncDeco[P, T]:
+    """Copies the doc string of the given function to another. 
+    This function is intended to be used as a decorator.
+
+    .. code-block:: python3
+
+        def foo():
+            '''This is a foo doc string'''
+            ...
+
+        @copy_doc(foo)
+        def bar():
+            ...
+    """
+
+    def wrapped(func: Callable[P, T]) -> Callable[P, T]:
+        func.__doc__ = copy_func.__doc__
+        return func
+
+    return wrapped
