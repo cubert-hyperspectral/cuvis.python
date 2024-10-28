755ec89d9654f2291fa67d41f6cd01124f2a3aa3
birkholz-cubert <142295759+birkholz-cubert@users.noreply.github.com>
2024-10-28 15:11:25 +0100

    Feature/fix viewer (#41)
    * added option to capture_async to add mesu to internal queue
    
    * added missing struct ViewerSettings
    
    * fix: missing argument in _create_view_data
    
    * fix: missing handle

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools.command import develop
 here = os.path.abspath(os.path.dirname(__file__))
 
 NAME = 'cuvis'
-VERSION = '3.3.0'
+VERSION = '3.3.0.post1'
 
 DESCRIPTION = 'CUVIS Python SDK.'
 
@@ -22,7 +22,7 @@ REQUIREMENTS = {
     # Installation dependencies
     # Use with pip install . to install from source
     'install': [
-        'cuvis-il == 3.3.0',
+        'cuvis-il>=3.3.0,<=3.3.0.post999999',
     ],
 }
 
