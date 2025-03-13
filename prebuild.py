291780a2359d74a72a1050354db506e21c9a4b8b
Simon Birkholz <birkholz@cubert-gmbh.de>
2025-03-13 12:53:08 +0100

    added prebuild.py script

---
+++ prebuild.py
@@ -0,0 +1,15 @@
+import subprocess
+from pathlib import Path
+
+
+def get_git_commit_hash():
+    try:
+        return subprocess.check_output(["git", "rev-parse", "HEAD"]).decode("ascii").strip()
+    except subprocess.CalledProcessError:
+        return "unknown"
+
+
+with open(Path(__file__).parent / "git-hash.txt", "w") as f:
+    f.write(f"{get_git_commit_hash()}\n")
+
+print("git-hash.txt created.")
